5c825a619fbfafc888be606ceab01da35af6a755
TheDragonDentist <65592198+TheDragonDentist@users.noreply.github.com>
2020-05-30 07:44:02 +0300

    Update dictionary.py

--- tg_bot/modules/dictionary.py
+++ tg_bot/modules/dictionary.py
@@ -34,7 +34,7 @@ With this module, you can find the definitions of words without having to leave 
  - /define <word>: returns the definition of the word.
  """
  
-__mod_name__ = "DICTIONARY"
+__mod_name__ = "Dictionary"
         
         
 DEFINE_HANDLER = CommandHandler("define", define, pass_args=True)
